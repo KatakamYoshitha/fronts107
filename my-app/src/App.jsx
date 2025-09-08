6906ee2b7ed741157cae5e21b0e38ef62a621a5a
KatakamYoshitha <yoshithaka123@gmail.com>
2025-09-08 12:24:46 +0530

    added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,3 +1,21 @@
+const Home = () => {
+  return (<div>
+    <h2>Home Component!</h2>
+    </div>
+    )
+}
+const About = () => {
+  return (<div>
+    <h2>About Component!</h2>
+    </div>
+    )
+}
+const Contact = () => {
+  return (<div>
+    <h2>Contact Component!</h2>
+    </div>
+    )
+}
 const App = () => {
   const d=new Date()
   console.log(d)
@@ -9,7 +27,11 @@ const App = () => {
       <h1>Congratulations, you completed your first React app!</h1>
       <p>Hello Time = {d.toString()}</p>
       <p> {a} + {b} = {a+b} </p>
+      <Home />
+      <About />
+      <Contact />
     </div>
   )
+  
 } 
-export default App
+export default App;
